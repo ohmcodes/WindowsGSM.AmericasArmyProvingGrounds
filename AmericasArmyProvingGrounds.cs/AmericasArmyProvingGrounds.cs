514965f1f07273a25bdfae4d97c5f8e72262c838
✪Ohmcodes™✪ <ohmcodes@gmail.com>
2024-02-14 11:15:05 -0800

    check

---
+++ AmericasArmyProvingGrounds.cs/AmericasArmyProvingGrounds.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using WindowsGSM.Functions;
+using WindowsGSM.GameServer.Engine;
+using WindowsGSM.GameServer.Query;
+using System.Collections.Generic;
+
+namespace WindowsGSM.Plugins
+{
+    public class AmericasArmyProvingGrounds : SteamCMDAgent
+    {
+        // - Plugin Details
+        public Plugin Plugin = new Plugin
+        {
+            name = "WindowsGSM.AmericasArmyProvingGrounds", // WindowsGSM.XXXX
+            author = "ohmcodes",
+            description = "WindowsGSM plugin for supporting Americas Army Proving Grounds Dedicated Server",
+            version = "1.0.0",
+            url = "https://github.com/ohmcodes/WindowsGSM.AmericasArmyProvingGrounds", // Github repository link (Best practice)
+            color = "#1E4359" // Color Hex
+        };
+
+        // - Standard Constructor and properties
+        public AmericasArmyProvingGrounds(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        private readonly ServerConfig _serverData;
+        public string Error, Notice;
+
+        // - Settings properties for SteamCMD installer
+        public override bool loginAnonymous => true;
+        public override string AppId => "203300"; /* taken via https://steamdb.info/app/203300/ */
+
+        // - Game server Fixed variables
+        public override string StartPath => @"Binaries\AALauncher32.exe"; // Game server start path
+        public string FullName = "Americas Army Proving Grounds Dedicated Server"; // Game server FullName
+        public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
+        public int PortIncrements = 0; // This tells WindowsGSM how many ports should skip after installation
+        public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
+
+        // - Game server default values
+        public string ServerName = "WGSM Americas Army Proving Grounds";
+        public string Defaultmap = ""; // Original (MapName)
+        public string Maxplayers = "2"; // WGSM reads this as string but originally it is number or int (MaxPlayers)
+        public string Port = "7777"; // WGSM reads this as string but originally it is number or int
+        public string QueryPort = "27015"; // WGSM reads this as string but originally it is number or int (SteamQueryPort)
+        public string Additional = "";
+
+        // - Create a default cfg for the game server after installation
+        public async void CreateServerCFG()
+        {
+
+        }
+
+        // - Start server function, return its Process to WindowsGSM
+        public async Task<Process> Start()
+        {
+            string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
+            if (!File.Exists(shipExePath))
+            {
+                Error = $"{Path.GetFileName(shipExePath)} not found ({shipExePath})";
+                return null;
+            }
+
+            var param = new StringBuilder();
+
+            param.Append($"{_serverData.ServerMap}");
+            param.Append($"?MaxPlayers={_serverData.ServerMaxPlayer}");
+
+            if (_serverData.ServerParam.StartsWith("?"))
+                param.Append($"{_serverData.ServerParam}");
+            else if (_serverData.ServerParam.StartsWith("-"))
+                param.Append($" {_serverData.ServerParam}");
+
+            param.Append($" -multihome={_serverData.ServerIP}");
+            param.Append($" -Port={_serverData.ServerPort}");
+
+
+            // Prepare Process
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+                    FileName = shipExePath,
+                    Arguments = param.ToString(),
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    UseShellExecute = false
+
+                },
+                EnableRaisingEvents = true
+            };
+
+            // Set up Redirect Input and Output to WindowsGSM Console if EmbedConsole is on
+            if (AllowsEmbedConsole)
+            {
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                var serverConsole = new ServerConsole(_serverData.ServerID);
+                p.OutputDataReceived += serverConsole.AddOutput;
+                p.ErrorDataReceived += serverConsole.AddOutput;
+            }
+
+            // Start Process
+            try
+            {
+                p.Start();
+                if (AllowsEmbedConsole)
+                {
+                    p.BeginOutputReadLine();
+                    p.BeginErrorReadLine();
+                }
+
+                return p;
+            }
+            catch (Exception e)
+            {
+                Error = e.Message;
+                return null; // return null if fail to start
+            }
+        }
+
+        // - Stop server function
+        public async Task Stop(Process p)
+        {
+            await Task.Run(() =>
+            {
+                Functions.ServerConsole.SetMainWindow(p.MainWindowHandle);
+                Functions.ServerConsole.SendWaitToMainWindow("^c");
+            });
+            await Task.Delay(2000);
+        }
+
+        // - Update server function
+        public async Task<Process> Update(bool validate = false, string custom = null)
+        {
+            var (p, error) = await Installer.SteamCMD.UpdateEx(serverData.ServerID, AppId, validate, custom: custom, loginAnonymous: loginAnonymous);
+            Error = error;
+            await Task.Run(() => { p.WaitForExit(); });
+            return p;
+        }
+
+        public bool IsInstallValid()
+        {
+            return File.Exists(Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath));
+        }
+
+        public bool IsImportValid(string path)
+        {
+            string exePath = Path.Combine(path, "PackageInfo.bin");
+            Error = $"Invalid Path! Fail to find {Path.GetFileName(exePath)}";
+            return File.Exists(exePath);
+        }
+
+        public string GetLocalBuild()
+        {
+            var steamCMD = new Installer.SteamCMD();
+            return steamCMD.GetLocalBuild(_serverData.ServerID, AppId);
+        }
+
+        public async Task<string> GetRemoteBuild()
+        {
+            var steamCMD = new Installer.SteamCMD();
+            return await steamCMD.GetRemoteBuild(AppId);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # WindowsGSM.AmericasArmyProvingGrounds
 🧩 WindowsGSM plugin that provides America's Army: Proving Grounds Dedicated server support!
+
+# WindowsGSM Installation: 
+1. Download  WindowsGSM https://windowsgsm.com/ 
+2. Create a Folder at a Location you wan't all Server to be Installed and Run.
+4. Drag WindowsGSM.exe into previously created folder and execute it.
+
+# Plugin Installation:
+1. Download [latest](https://github.com/ohmcodes/WindowsGSM.AmericasArmyProvingGrounds/releases/latest) release
+2. Extract then Move **AmericasArmyProvingGrounds.cs** folder to **plugins** folder
+3. OR Press on the Puzzle Icon in the left bottom side and install this plugin by navigating to it and select the Zip File.
+4. Click **[RELOAD PLUGINS]** button or restart WindowsGSM
+5. Navigate "Servers" and Click "Install Game Server" and find "Americas Army Proving Grounds Dedicated Server [AmericasArmyProvingGrounds.cs]
+
+### Official Documentation
+🗃️ None
+
+### Unofficial Documentation
+🗃️ https://steamcommunity.com/app/203290/discussions/6/846963165509522965
+
+### The Game
+🕹️ https://store.steampowered.com/app/203290/Americas_Army_Proving_Grounds/
+
+### Dedicated server info
+🖥️ https://steamdb.info/app/239510/
+
+
+# License
+This project is licensed under the MIT License - see the <a href="https://github.com/ohmcodes/WindowsGSM.AmericasArmyProvingGrounds/blob/main/LICENSE">LICENSE.md</a> file for details
